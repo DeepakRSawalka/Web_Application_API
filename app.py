6ecfcbdb5dd5e9ad71e6b85b59361413737fd2b3
Deepak Sawalka <Sawalka.d@northeastern.edu>
2024-03-27 12:42:18 -0400

    modify files

--- app.py
+++ app.py
@@ -13,29 +13,11 @@ import uuid
 import pandas as pd
 import statsd
 from app_logging import logger
-import logging
 
-
-logging.basicConfig(level=logging.INFO)
-
-logging.info(f"After loading .env: DBHOST={os.getenv('DBHOST')}")
-
-db_host = os.getenv("DBHOST")
-print(db_host)
-db_port = os.getenv("DBPORT")
-print(db_port)
-db_user = os.getenv("DBUSER")
-print(db_user)
-db_pass = os.getenv("DBPASS")
-print(db_pass)
-database = os.getenv("DATABASE")
-print(database)
 
 app = Flask(__name__)
 
-database_uri = f"postgresql+psycopg2://{os.getenv('DBUSER')}:{os.getenv('DBPASS')}@{os.getenv('DBHOST')}:{os.getenv('DBPORT')}/{os.getenv('DATABASE')}"
-print(f"Database URI: {database_uri}")
-app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql+psycopg2://{os.getenv('DBUSER')}:{os.getenv('DBPASS')}@{os.getenv('DBHOST')}:{os.getenv('DBPORT')}/{os.getenv('DATABASE')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -47,19 +29,30 @@ c = statsd.StatsClient('localhost', 8125)
 
 @app.route('/healthz', methods = ['GET'])
 def healthcheck():
+    c.incr('healthz_counter')
+    if request.args:
+        logger.error("Query parameters are not allowed", extra={'method': 'GET', 'uri': '/healthz', 'statusCode': 400})
+        return jsonify({"message": "Query parameters are not allowed"}), 400
     try:
+        if request.data:
+            logger.error("Request body should be empty", extra={'method': 'GET', 'uri': '/healthz', 'statusCode': 400})
+            return jsonify({"message": "Request body should be empty"}), 400
+        
         db.session.execute('SELECT 1')
-        res = make_response(jsonify(''), 200)
+        res = make_response(jsonify({"message" : "Endpoint is healthy"}), 200)
+        logger.info("healthz is working fine", extra={'method': 'GET', 'uri': '/healthz', 'statusCode': 200})
 
     except Exception as e:
-        res = make_response(jsonify(''), 503)
+        res = make_response(jsonify({f"message" : {e}}), 503)
+        logger.error("healthz is not working server unavailable", extra={'method': 'GET', 'uri': '/healthz', 'statusCode': 503})
     
     res.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, proxy-revalidate'
     return res
 
 @app.route('/healthz', methods = ['POST','PUT','DELETE','PATCH'])
 def check():
-    res = make_response(jsonify(''), 405)
+    res = make_response(jsonify({"message" : "Method not allowed"}), 405)
+    logger.error("Change the method to GET (Method not allowed)", extra={'method': 'GET', 'uri': '/healthz', 'statusCode': 405})
     return res
 
 def add_users():
@@ -71,11 +64,11 @@ def add_users():
         # Iterate over DataFrame rows
         for index, row in df.iterrows():
             if not all([row['first_name'], row['last_name'], row['email'], row['password']]):
-                #logger.error('Enter Valid User data')
+                logger.error( "Enter Valid User data", extra={'statusCode': 400})
                 return jsonify(message='Enter Valid User data'), 400
 
             if Users.query.filter_by(email=row['email']).first():
-                #logger.info('User with the same email already exists')
+                logger.info("User with the same email already exists")
                 continue
 
             hashed_password = Encryption.encrypt(row['password'])
@@ -89,33 +82,37 @@ def add_users():
             
 
         db.session.commit()
-        #logger.info('Users added successfully')
+        logger.info("Users added successfully", extra={'statusCode': 201})
         print("Users added successfully")
         
         return jsonify({"message" : "Users added successfully"}), 201
 
     except Exception as e:
-        #logger.error(f'DB error: {e}')
+        logger.error(f'Server error: {e}', extra={'statusCode': 500})
         return jsonify(message=f'DB error: {e}'), 500
   
 @app.route('/v1/assignments', methods = ['POST'])
 def create_assignment():
+    c.incr('Create_assignments')
     auth_header = request.headers.get('Authorization')
     if not auth_header:
+        logger.error("Authentication required", extra={'method': 'POST', 'uri': '/v1/assignments', 'statusCode': 401})
         return jsonify({"message": "Authentication required"}), 401
     
     email, password = Encryption.decode(auth_header)
     if not Validation.validate_email(email):
+        logger.error("Invalid email format", extra={'method': 'POST', 'uri': '/v1/assignments', 'statusCode': 400})
         return jsonify({"message": "Invalid email format"}), 400
     
     user = Validation.validate_user(email, password)
     if not user:
+        logger.error("Invalid credentials-Unauthorised", extra={'method': 'POST', 'uri': '/v1/assignments', 'statusCode': 401})
         return jsonify({"message" : "Invalid credentials-Unauthorised"}), 401
     
     data = request.get_json()
     message = Validation.isAssignDataValid(data)
     if message != "":
-        # logger.error(message)
+        logger.error(message, extra={'method': 'POST', 'uri': '/v1/assignments', 'statusCode': 400})
         return jsonify({"message" : message}), 400
     
     name = data.get("name")
@@ -124,7 +121,7 @@ def create_assignment():
     deadline = data.get("deadline")
     assign = Assignments.query.filter_by(name=name).first()
     if assign:
-        # logger.error('User already exist')
+        logger.error("Assignment already exist", extra={'method': 'POST', 'uri': '/v1/assignments', 'statusCode': 400})
         return jsonify({"message":"Assignment already exist"}), 400
 
     new_assign = Assignments(name=name, points=points, num_of_attempts=num_of_attempts, deadline=deadline, owner_user_id=user.id)
@@ -141,33 +138,39 @@ def create_assignment():
             "assignment_created": new_assign.assignment_created,
             "assignment_updated": new_assign.assignment_updated
         } 
-        # logger.info('Create User API ended')
+        logger.info(schema, extra={'method': 'POST', 'uri': '/v1/assignments', 'statusCode': 201})
         return schema,201
     except SQLAlchemyError as e:
         db.session.rollback()  # Roll back the session on error
-        # logger.error(f"Database error: {str(e)}")
-        return jsonify({"message": "Database error, could not create assignment"}), 500
+        logger.error(f"Database error, could not create assignment - {e}", extra={'method': 'POST', 'uri': '/v1/assignments', 'statusCode': 500})
+        return jsonify({"message": f"Database error, could not create assignment - {e}"}), 500
     
 @app.route('/v1/assignments', methods = ['GET'])
 def get_assignments():
+    c.incr('GET_assignment_list')
     auth_header = request.headers.get('Authorization')
     if not auth_header:
+        logger.error("Authentication required", extra={'method': 'GET', 'uri': '/v1/assignments', 'statusCode': 401})
         return jsonify({"message": "Authentication required"}), 401
     
     email, password = Encryption.decode(auth_header)
     if not Validation.validate_email(email):
+        logger.error("Invalid email format", extra={'method': 'GET', 'uri': '/v1/assignments', 'statusCode': 400})
         return jsonify({"message": "Invalid email format"}), 400
     
     user = Validation.validate_user(email, password)
     if not user:
+        logger.error("Invalid credentials-Unauthorised", extra={'method': 'GET', 'uri': '/v1/assignments', 'statusCode': 401})
         return jsonify({"message" : "Invalid credentials-Unauthorised"}), 401
     
     if request.data:
+        logger.error("Request body should be empty", extra={'method': 'GET', 'uri': '/v1/assignments', 'statusCode': 400})
         return jsonify({"message": "Request body should be empty"}), 400
     
     try:
         assignments = Assignments.query.all() 
         if not assignments:
+            logger.error("Assignment not found", extra={'method': 'GET', 'uri': '/v1/assignments', 'statusCode': 404})
             return jsonify({"message": "Assignment not found"}), 404
          
         schema = []
@@ -182,33 +185,39 @@ def get_assignments():
             "assignment_updated": assignment.assignment_updated.isoformat()
          }
             schema.append(result)
-
+        logger.info(schema, extra={'method': 'GET', 'uri': '/v1/assignments', 'statusCode': 200})
         return schema,200
 
     except Exception as e:
-        # logger.error(f"Database error: {str(e)}")
+        logger.error(f"Server error: {e}", extra={'method': 'GET', 'uri': '/v1/assignments', 'statusCode': 500})
         return jsonify({"message": f"Server error: {e}"}), 500
 
 @app.route('/v1/assignments/<id>', methods = ['GET'])
 def get_assignments_details(id):
+    c.incr('GET_assignment_details')
     auth_header = request.headers.get('Authorization')
     if not auth_header:
+        logger.error("Authentication required", extra={'method': 'GET', 'uri': '/v1/assignments/'+ id, 'statusCode': 401})
         return jsonify({"message": "Authentication required"}), 401
     
     email, password = Encryption.decode(auth_header)
     if not Validation.validate_email(email):
+        logger.error("Invalid email format", extra={'method': 'GET', 'uri': '/v1/assignments/'+ id, 'statusCode': 400})
         return jsonify({"message": "Invalid email format"}), 400
     
     user = Validation.validate_user(email, password)
     if not user:
+        logger.error("Invalid credentials-Unauthorised", extra={'method': 'GET', 'uri': '/v1/assignments/'+ id, 'statusCode': 401})
         return jsonify({"message" : "Invalid credentials-Unauthorised"}), 401
     
     if request.data:
+        logger.error("Request body should be empty", extra={'method': 'GET', 'uri': '/v1/assignments/'+ id, 'statusCode': 400})
         return jsonify({"message": "Request body should be empty"}), 400
     
     try:
         assignments = Assignments.query.filter_by(id=id).first()
         if not assignments:
+            logger.error("Assignment not found", extra={'method': 'GET', 'uri': '/v1/assignments/'+ id, 'statusCode': 404})
             return jsonify({"message": "Assignment not found"}), 404
          
         schema = {
@@ -220,40 +229,47 @@ def get_assignments_details(id):
         "assignment_created": assignments.assignment_created.isoformat(),
         "assignment_updated": assignments.assignment_updated.isoformat()
          }
-            
+        
+        logger.info(schema, extra={'method': 'GET', 'uri': '/v1/assignments/'+ id, 'statusCode': 200})  
         return schema,200
 
     except Exception as e:
-        # logger.error(f"Database error: {str(e)}")
+        logger.error(f"Server error: {e}", extra={'method': 'GET', 'uri': '/v1/assignments/'+ id, 'statusCode': 500})
         return jsonify({"message": f"Server error: {e}"}), 500
 
 @app.route('/v1/assignments/<id>', methods = ['PUT'])
 def update_assignments(id):
+    c.incr('Update_assignments')
     auth_header = request.headers.get('Authorization')
     if not auth_header:
+        logger.error("Authentication required", extra={'method': 'PUT', 'uri': '/v1/assignments/'+ id, 'statusCode': 401})
         return jsonify({"message": "Authentication required"}), 401
     
     email, password = Encryption.decode(auth_header)
     if not Validation.validate_email(email):
+        logger.error("Invalid email format", extra={'method': 'PUT', 'uri': '/v1/assignments/'+ id, 'statusCode': 400})
         return jsonify({"message": "Invalid email format"}), 400
     
     user = Validation.validate_user(email, password)
     if not user:
+        logger.error("Invalid credentials-Unauthorised", extra={'method': 'PUT', 'uri': '/v1/assignments/'+ id, 'statusCode': 401})
         return jsonify({"message" : "Invalid credentials-Unauthorised"}), 401
     
     try:
         assignments = Assignments.query.filter_by(id=id).first()
 
         if assignments.owner_user_id != user.id:
+            logger.error("User does not have necessary permissions to Update-Forbidden", extra={'method': 'PUT', 'uri': '/v1/assignments/'+ id, 'statusCode': 403})
             return jsonify({"message": "User does not have necessary permissions to Update-Forbidden"}), 403
 
         if not assignments:
+            logger.error("Assignment not found", extra={'method': 'PUT', 'uri': '/v1/assignments/'+ id, 'statusCode': 404})
             return jsonify({"message": "Assignment not found"}), 404
         
         data = request.get_json()
         message = Validation.isAssignDataValid(data)
         if message != "":
-            # logger.error(message)
+            logger.error(message, extra={'method': 'PUT', 'uri': '/v1/assignments/'+ id, 'statusCode': 400})
             return jsonify({"message" : message}), 400
     
         assignments.name = data.get("name")
@@ -262,51 +278,62 @@ def update_assignments(id):
         assignments.deadline = data.get("deadline")
          
         db.session.commit()
-            
+
+        logger.info("Assignment updated Successfully!!", extra={'method': 'PUT', 'uri': '/v1/assignments/'+ id, 'statusCode': 204}) 
         return {},204
 
     except Exception as e:
-        # logger.error(f"Database error: {str(e)}")
+        logger.error(f"Server error: {e}", extra={'method': 'PUT', 'uri': '/v1/assignments/'+ id, 'statusCode': 500})
         return jsonify({"message": f"Server error: {e}"}), 500
     
 
 @app.route('/v1/assignments/<id>', methods = ['DELETE'])
 def delete_assignments(id):
+    c.incr('Delete_assignments')
     auth_header = request.headers.get('Authorization')
     if not auth_header:
+        logger.error("Authentication required", extra={'method': 'DELETE', 'uri': '/v1/assignments/'+ id, 'statusCode': 401})
         return jsonify({"message": "Authentication required"}), 401
     
     email, password = Encryption.decode(auth_header)
     if not Validation.validate_email(email):
+        logger.error("Invalid email format", extra={'method': 'DELETE', 'uri': '/v1/assignments/'+ id, 'statusCode': 400})
         return jsonify({"message": "Invalid email format"}), 400
     
     user = Validation.validate_user(email, password)
     if not user:
+        logger.error("Invalid credentials-Unauthorised", extra={'method': 'DELETE', 'uri': '/v1/assignments/'+ id, 'statusCode': 401})
         return jsonify({"message" : "Invalid credentials-Unauthorised"}), 401
     
     try:
         assignments = Assignments.query.filter_by(id=id).first()
 
         if assignments.owner_user_id != user.id:
+            logger.error("User does not have necessary permissions to Delete-Forbidden", extra={'method': 'DELETE', 'uri': '/v1/assignments/'+ id, 'statusCode': 403})
             return jsonify({"message": "User does not have necessary permissions to Delete-Forbidden"}), 403
 
         if request.data:
+            logger.error("Request body should be empty", extra={'method': 'DELETE', 'uri': '/v1/assignments/'+ id, 'statusCode': 400})
             return jsonify({"message": "Request body should be empty"}), 400
     
         if not assignments:
+            logger.error("Assignment not found", extra={'method': 'DELETE', 'uri': '/v1/assignments/'+ id, 'statusCode': 404})
             return jsonify({"message": "Assignment not found"}), 404
 
         db.session.delete(assignments)
         db.session.commit()
-            
+        
+        logger.info("Assignment Deleted Successfully!!", extra={'method': 'DELETE', 'uri': '/v1/assignments/'+ id, 'statusCode': 204})
         return {},204
 
     except Exception as e:
-        # logger.error(f"Database error: {str(e)}")
+        logger.error(f"Server error: {e}", extra={'method': 'DELETE', 'uri': '/v1/assignments/'+ id, 'statusCode': 500})
         return jsonify({"message": f"Server error: {e}"}), 500
     
 @app.route('/v1/assignments/<id>', methods = ['PATCH'])
 def update(id):
+    c.incr('Patch_assignments')
+    logger.error("Method Not allowed", extra={'method': 'PATCH', 'uri': '/v1/assignments/'+ id, 'statusCode': 405})
     return {},405
     
 if __name__ == '__main__':
